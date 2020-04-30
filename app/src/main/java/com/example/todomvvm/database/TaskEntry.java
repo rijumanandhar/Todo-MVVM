2cd1d559ab01a107718e2c578d7920de0d9676b0
Riju Manandhar <rijumd@gmail.com>
2020-04-30 19:46:59 +0545

    Entity Created

--- app/src/main/java/com/example/todomvvm/database/TaskEntry.java
+++ app/src/main/java/com/example/todomvvm/database/TaskEntry.java
@@ -1,17 +1,27 @@
 package com.example.todomvvm.database;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
 import java.util.Date;
 
 // TODO (2) Annotate the class with Entity. Use "task" for the table name
+@Entity (tableName = "Task")
 public class TaskEntry {
 
     // TODO (3) Annotate the id as PrimaryKey. Set autoGenerate to true.
+    @PrimaryKey(autoGenerate = true)
     private int id;
     private String description;
+
     private int priority;
+    @ColumnInfo(name = "updated_at")
     private Date updatedAt;
 
     // TODO (4) Use the Ignore annotation so Room knows that it has to use the other constructor instead
+    @Ignore
     public TaskEntry(String description, int priority, Date updatedAt) {
         this.description = description;
         this.priority = priority;
